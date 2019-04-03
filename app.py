99645e1d027b8fcba03695621d93321c6794ae3c
mugi232 <thenmugilan.gandhy@gmail.com>
2019-04-03 10:56:16 +0800

    Added project/product section.Added the route for each product.

--- app.py
+++ app.py
@@ -11,6 +11,18 @@ def index():
 def elements_theme():
     return render_template("elements.html")
 
+@app.route('/marketplace')
+def marketplace():
+    return render_template("project-marketplace-details.html")
+
+@app.route('/logistics')
+def logistics():
+    return render_template("project-logistics-details.html")
+
+@app.route('/scrapper')
+def scrapper():
+    return render_template("project-scrapper-details.html")
+
 # Run the web app
 
 if __name__ == '__main__':
